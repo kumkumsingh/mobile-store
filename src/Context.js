78cfad1f7bfc8478de76ebe4e5aeeb6fbaf91ede
kumkumsingh <kumkumsingh.r@gmail.com>
2020-01-02 19:11:45 +0100

    done with product list page

--- src/Context.js
+++ src/Context.js
@@ -4,9 +4,23 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
   };
+  componentDidMount(){
+      this.storeProducts();
+  }
+  storeProducts = () =>{
+      let tempProducts=[];
+      storeProducts.forEach(item =>{
+          const singleItem = {...item};
+          tempProducts = [...tempProducts,singleItem]
+      })
+      this.setState(() =>{
+          return {products:tempProducts}
+      })
+
+  }
   hanldeDetail = () => {
     console.log("hello from detail");
   };
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import {ProductConsumer} from '../Context'
+import {Link} from 'react-router-dom'
+import {ButtonContainer} from './Button'
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-               <h3>Hello from details</h3> 
-            </div>
+            <ProductConsumer>
+              {(value) => {
+                 const {id,company,img,info,price,title,inCart} = value.detailProduct
+                 return(<div className="container py-5">
+                     <div className="row">
+                         <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                             <h1>{title}</h1>
+                         </div>
+                     </div>
+                 </div>)
+
+              }}
+            </ProductConsumer>
         )
     }
 }

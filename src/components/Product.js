b415f900c80ba803a052e6f01acadc34e4f0edf5
Rajmohankct2003 <rajmohankct2003@gmail.com>
2020-01-02 09:35:15 +0100

    added products in the page with hooks and context

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import {ProductCosumer} from '../Context'
 
 export default class Product extends Component {
     render() {
+        const { id, title, img, price, inCart} = this.props.product
         return (
-            <div>
-                hello from product
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+               <div className="card">
+                   <div className="image-container p-5" onClick={()=>console.log("you clicked me on ")}>
+                       <Link to='/details'>
+                           <img src={img} alt="product" className="card-img-top"></img>
+                       </Link>
+                       <button className="card-btn" disabled={inCart?true:false} 
+                       onClick={() =>{console.log('added tothe cart')}}>
+                           {inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>):(<i className="fa fa-cart-plus"/>)}
+                       </button>
+                       
+                   </div>
+               </div>
+            </ProductWrapper>
         )
     }
 }
+
+const ProductWrapper = styled.div``
